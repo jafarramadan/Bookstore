71a28018207649401556ad47f357038bc86175ae
Ja'far Ramadan <23040271@student.ltuc.com>
2024-09-29 02:16:00 +0300

    Add project files.

---
+++ Bookstore/Data/BookstoreDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Bookstore.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bookstore.Data
+{
+    public class BookstoreDbContext : DbContext 
+    {
+        public BookstoreDbContext(DbContextOptions<BookstoreDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Book> Book { get; set; }
+
+    }
+}
---
+++ Bookstore/Models/Book.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Bookstore.Models
+{
+    public class Book
+    {
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string Title { get; set; }
+        [Required]
+        public string Author { get; set; }
+        [Required]
+        public string Price { get; set; }
+        [Required]
+        public string Genre { get; set; }
+
+    }
+}
